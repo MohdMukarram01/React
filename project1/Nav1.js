b7fbba61725b98bcd861cfbabadc2cc6af158667
MohdMukarram01 <147875144+MohdMukarram01@users.noreply.github.com>
2024-08-18 00:16:10 +0530

    Add files via upload

---
+++ project1/Nav1.js
@@ -0,0 +1,23 @@
+function Nav1() {
+  return (
+    <div className="container">
+    <div>
+        <nav>
+          <div>
+            <img src="/images/brand_logo.png" alt="logo" />
+          </div>
+          <div>
+            <ul>
+              <li href="#">Menu</li>
+              <li href="#">Location</li>
+              <li href="#">About</li>
+              <li href="#">Contact</li>
+            </ul>
+          </div>
+          <button>Login</button>
+        </nav>
+      </div>
+    </div>
+  );
+}
+export default Nav1 ;
